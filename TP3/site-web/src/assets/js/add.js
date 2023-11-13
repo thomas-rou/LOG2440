0cbb2ca0822c9f3426949f18b0c2229a8ead3ace
Thomas Rouleau <thomas.rouleau@polymtl.ca>
2023-11-13 18:18:51 -0500

    add.js : Récupération des infos, ajout du partenaire, redirection
    Done 100%

--- TP3/site-web/src/assets/js/add.js
+++ TP3/site-web/src/assets/js/add.js
@@ -1,29 +1,36 @@
 // TODO : Récupérer les bons éléments des fichiers externes
+import HTTPManager from "./HTTPManager.js";
+import SERVER_URL from "./consts.js";
 
-
+const httpManager = new HTTPManager(SERVER_URL);
 const submitButton = document.getElementById('submit-btn');
-submitButton.addEventListener('click', async (e) => {
+submitButton.addEventListener('click', (e) => {
     e.preventDefault();
-    const partnerFirstName = document.getElementById('first-name');
-    const partnerLastName = document.getElementById('last-name');
+    (async () => {
+        const partnerFirstName = document.getElementById('first-name');
+        const partnerLastName = document.getElementById('last-name');
+        const partnerProgram = document.getElementById('program');
+        const partnerSchool = document.getElementById('school');
 
-    // TODO : Compléter la création de l'objet du partenaire
+        // TODO : Compléter la création de l'objet du partenaire
+        const partner = {
+            firstName: partnerFirstName.value,
+            lastName: partnerLastName.value,
+            school: partnerSchool.value,
+            program: partnerProgram.value
+        };
 
-    const partner = {
-        firstName: partnerFirstName.value,
-        lastName: partnerLastName.value,
-    };
-
-    // TODO : Ajouter un nouveau partenaire à travers le serveur.
-    // TODO : Rediriger l'utilisateur vers index.html en cas de réussite ou /error.html en cas d'échec
-    try {
-        const response = { firstName : "TODO: Contacter le serveur" };
-        if (response) {
-            window.alert("L'étudiant avec le nom " + response.firstName + " a été créé !");
-            window.location.href = '/add.html';
+        // TODO : Ajouter un nouveau partenaire à travers le serveur.
+        // TODO : Rediriger l'utilisateur vers index.html en cas de réussite ou /error.html en cas d'échec
+        try {
+            const response = await httpManager.post('/api/partner', partner);
+            if (response) {
+                window.alert("L'étudiant avec le nom " + response.firstName + " a été créé !");
+                window.location.href = '/index.html';
+            }
+        } catch (error) {
+            alert("Échec de la création du partenaire ! - " + error.message);
+            window.location.href = '/error.html';
         }
-    } catch (error) {
-        alert("Échec de la création du partenaire ! - " + error.message);
-        window.location.href = '/add.html';
-    }
+    })();
 });
