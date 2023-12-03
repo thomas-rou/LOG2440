eff1f944f86528c5171378c6a25986d55c7f6b27
Thomas Rouleau <thomas.rouleau@polymtl.ca>
2023-12-02 23:38:50 -0500

    Footer.jsx : ajout de nos noms
    tests : 2/2

--- TP4/site-web/src/components/Footer.jsx
+++ TP4/site-web/src/components/Footer.jsx
@@ -21,8 +21,8 @@ export default function Footer() {
     <footer id="playing-bar">
       {loadPlayer(useLocation().pathname)}
       <div id="creators">
-        <p>étudiant.e 1 matricule</p>
-        <p>étudiant.e 2 matricule</p>
+        <p>Francis Trahan #2209296</p>
+        <p>Thomas Rouleau #2221053</p>
       </div>
     </footer>
   );
