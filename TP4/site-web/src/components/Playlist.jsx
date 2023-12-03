1e60ae2328cc27fb497039770e1b5c29bbdcd8a0
Thomas Rouleau <thomas.rouleau@polymtl.ca>
2023-12-03 00:21:11 -0500

    Playlist.jsx : affichage des informations des playlists dans Index.jsx
    Affichage de l'image, du nom et de la description des playlists
    
    Playlist tests : 2/2

--- TP4/site-web/src/components/Playlist.jsx
+++ TP4/site-web/src/components/Playlist.jsx
@@ -7,12 +7,12 @@ export default function Playlist({ playlist }) {
     <NavLink className="playlist-item flex-column" to={`/playlist/${playlist.id}`}>
       <div className="playlist-preview">
         {/*TODO : ajouter l'image de la playlist */}
-        <img alt="" src={`${SERVER_URL}/TODO`} />
+        <img alt="" src={`${SERVER_URL}/${playlist.thumbnail}`} />
         <i className="fa fa-2x fa-play-circle hidden playlist-play-icon"></i>
       </div>
       {/*TODO : ajouter les informations de la playlist */}
-      <p>TODO : Ma Première Playlist</p>
-      <p>TODO : Playlist de base</p>
+      <p>{playlist.name}</p>
+      <p>{playlist.description}</p>
     </NavLink>
   );
 }
